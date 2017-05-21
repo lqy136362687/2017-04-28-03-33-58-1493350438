da1cc7cc079f0563f0856c81cafffd6ad5d00359
2522824400@qq.com <2522824400@qq.com>
2017-05-21 14:07:08 +0800

    compute score

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -4,23 +4,24 @@ public class BowlingGame {
     int sumscore=0;
 		int length=bowlingCode.split("|").length;
 		int count=0;
+		char[] bowlingscore=bowlingCode.toCharArray();
 		int[] score=new int[length-10];
-		for(int i=0;i<length;i++){
-      String s=bowlingCode.split("|")[i];
+		for(int i=0;i<length;i++){//转为每个球打的分数
+			char s=bowlingscore[i];
 			switch(s){
-			case "X":
+			case 'X':
 				score[count]=10;
 				count++;
 				break;
-			case "/":
+			case '/':
 				score[count]=10-score[count-1];
 				count++;
 				break;
-			case "-":
+			case '-':
 				score[count]=0;
 				count++;
 				break;
-			case "|":
+			case '|':
 				break;
 			default:
 				score[count]=Integer.parseInt(bowlingCode.split("|")[i]);
@@ -30,7 +31,7 @@ public class BowlingGame {
 		}
 		int num=0;
 		for(int i=0;i<10;i++){
-			switch(score[num]){
+			switch(score[num]){//计算总分
 			case 10:
 				sumscore+=10;
 				sumscore+=score[num+1]+score[num+2];
