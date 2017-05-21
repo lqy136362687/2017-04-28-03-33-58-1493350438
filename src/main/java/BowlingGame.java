ca6bec1b9da4a4055c04ed43ea2296a0f338a3cd
2522824400@qq.com <2522824400@qq.com>
2017-05-21 13:41:27 +0800

    compute score

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,51 @@
 public class BowlingGame {
 
-    public int getBowlingScore(String bowlingCode) {
-        return 0;
+  public int getBowlingScore(String bowlingCode) {
+    int sumscore=0;
+		int length=bowlingCode.split("|").length;
+		int count=0;
+		int[] score=new int[length-10];
+		for(int i=0;i<length;i++){
+			switch(bowlingCode.split("|")[i]){
+			case "X":
+				score[count]=10;
+				count++;
+				break;
+			case "/":
+				score[count]=10-score[count-1];
+				count++;
+				break;
+			case "-":
+				score[count]=0;
+				count++;
+				break;
+			case "|":
+				break;
+			default:
+				score[count]=Integer.parseInt(bowlingCode.split("|")[i]);
+				count++;
+				break;
+			}
+		}
+		int num=0;
+		for(int i=0;i<10;i++){
+			switch(score[num]){
+			case 10:
+				sumscore+=10;
+				sumscore+=score[num+1]+score[num+2];
+				num++;
+				break;
+			default:
+				if(score[num]+score[num+1]==10){
+					sumscore+=10+score[num+2];
+					num+=2;
+				}else{
+					sumscore+=score[num]+score[num+1];
+					num+=2;
+				}
+			}
+		}
+		System.out.println(sumscore);
+	    return sumscore;
     }
 }
